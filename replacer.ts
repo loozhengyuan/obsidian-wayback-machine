01e2f9a779e1ce5d423fbf23c069910ce28e5053
Loo Zheng Yuan <loozhengyuan@gmail.com>
2025-08-21 12:18:30 +1200

    refactor: link validity logic

--- replacer.ts
+++ replacer.ts
@@ -3,21 +3,13 @@ import { WaybackMachineClient } from "./wayback.ts";
 // TODO: Avoid links from matching prematurely?
 export const URL_REGEX = /https?:\/\/[^\s\)>\]]+/g;
 
-export const HOSTNAMES_BLOCKED = [
-  "0.0.0.0",
-  // Localhost
-  "localhost",
-  "127.0.0.1",
-  "::1",
-  // Wayback Machine
-  "web.archive.org",
-];
-
 export type LinkProcessCallback = (link: string) => void;
+export type LinkIgnoreCallback = (link: string) => void;
 export type LinkReplaceCallback = (link: string) => void;
 
 export type LinkReplacerOptions = {
   onLinkProcess?: LinkProcessCallback;
+  onLinkIgnore?: LinkIgnoreCallback;
   onLinkReplace?: LinkReplaceCallback;
 };
 
@@ -27,11 +19,13 @@ export type LinkReplacerOptions = {
 export class LinkReplacer {
   private client: WaybackMachineClient;
   private onLinkProcess?: LinkProcessCallback;
+  private onLinkIgnore?: LinkIgnoreCallback;
   private onLinkReplace?: LinkReplaceCallback;
 
   constructor(client: WaybackMachineClient, options?: LinkReplacerOptions) {
     this.client = client;
     this.onLinkProcess = options?.onLinkProcess;
+    this.onLinkIgnore = options?.onLinkIgnore;
     this.onLinkReplace = options?.onLinkReplace;
   }
 
@@ -41,18 +35,19 @@ export class LinkReplacer {
    * @returns Promise resolving to the content with replaced links
    */
   async replace(content: string): Promise<string> {
-    const links = [...new Set(content.match(URL_REGEX) || [])];
-    if (links.length === 0) {
+    const matches = content.match(URL_REGEX);
+    if (!matches) {
       return content;
     }
+    const links = matches.unique();
 
     let result = content;
     for (const link of links) {
       this.onLinkProcess?.(link);
 
-      const url = new URL(link);
-      if (HOSTNAMES_BLOCKED.includes(url.hostname)) {
-        console.warn(`Ignoring blocked hostname: ${url.hostname}`);
+      if (!this.isLinkValid(link)) {
+        console.warn(`Ignoring hostname: ${link}`);
+        this.onLinkIgnore?.(link);
         continue;
       }
 
@@ -73,5 +68,23 @@ export class LinkReplacer {
     }
 
     return result;
+  }
+
+  private isLinkValid(link: string): boolean {
+    const url = new URL(link);
+    if (!url.hostname || url.hostname.trim() === "") {
+      return false;
+    }
+    if (
+      url.hostname === "localhost" ||
+      url.hostname === "127.0.0.1" ||
+      url.hostname === "::1"
+    ) {
+      return false;
+    }
+    if (url.hostname === "web.archive.org") {
+      return false;
+    }
+    return true;
   }
 }
